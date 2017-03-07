37287cb38c3da93e5354bf80f27edec9757caa5f
elizeon <l.haynes070@gmail.com>
2017-03-07 22:06:10 +0800

    Planning world lua

---
+++ OpenGLEngine/OpenGLEngine/World.lua
@@ -0,0 +1,38 @@
+world =
+{
+	local player =
+	{
+
+	}
+	
+	bodies = {}
+	
+	
+	local function world:enterScene(sceneToEnter)
+	{
+	
+	}
+	
+	local function world:loadSceneFromFile(sceneFile) return 'sceneIndex'
+	{
+	}
+}
+
+scene =
+{
+	bodies = {}
+	
+	local function addStatic()
+	
+	local function addPickUp()
+	
+	
+}
+
+
+RigidBody
+InteractableBody = RigidBody:New()
+NPCBody = InteractableBody:New()
+PickUpBody = InteractableBody:New()
+
+
