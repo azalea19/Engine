a21d44f02f3d687e9cf3c904a290ccad10971b7e
elizeon <l.haynes070@gmail.com>
2017-05-05 11:12:10 +0800

    Added lua text to display API.

--- OpenGLEngine/OpenGLEngine/LuaDisplay2DAPI.h
+++ OpenGLEngine/OpenGLEngine/LuaDisplay2DAPI.h
@@ -1,16 +1,26 @@
 #pragma once
+#include "Types.h"
+
 #include "FrameBuffer.h"
 
 #include "LuaContext.h"
 #include "LuaManager.h"
+#include <GL/glew.h>
+#include "MathAPI.h"
+
 #include "TextureLibrary.h"
 #include "Texture.h"
+#include "Interface2D.h"
+
+#include "LuaBridge.h"
+
 
 
 class LuaDisplay2DAPI
 {
 public:
 	static void LuaDisplay2DAPI::DrawFullScreen(string filePath);
+	static void LuaDisplay2DAPI::DrawTextLua(int size, string const& filePath, string const& text, int xpos, int ypos, LuaRef color);
 	static void LuaDisplay2DAPI::Expose(LuaContextHandle contextHandle, string luaAPIName);
 
 };
